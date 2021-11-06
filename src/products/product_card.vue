a0da68c12654f1045ca44a01153b68beacb3ceff
aKharkov <aKharkov@mail.ru>
2021-11-06 23:44:32 +0300

    CoffeTea bootstrap and  vue components products and card start

--- src/products/product_card.vue
+++ src/products/product_card.vue
@@ -2,19 +2,23 @@
 
 <template>
     
-            <div class="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-2">
+            <div id="mainCardDiv" class="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-2">
                 <div class="row">
-                    <div id="cardLogo" class="col-3"  >
-                   
+                    <div id="cardLogo" class="col-3" :style="{'background-image':'url('+Item.productPic+')'}"  >
+                         <!-- background: url("../img/coffee/coffee1.jpg") no-repeat center top; --> 
+                        
+                        <!-- <img :src="Item.productPic"  alt="VUE">  -->
+                        <!-- <img src="../img/coffee/coffee1.jpg"  >  -->
                     </div>
                     <div id=CardTitle class="col-9"> 
-
+                        {{Item.productProp}}
+                        
                     </div>
 
                 </div>
                 <div class="row">
 
-                    bssbvdsbvj
+                    {{productProp}}
                 </div>
 
                 <button> Подробнее </button>
@@ -27,19 +31,50 @@
 <script>
 
 export default {
+    props:{Item: Object},
+    data: function( ){
+    
+        return{
+             cardId:"",
+            productType:"",
+            productId:"",
+            productTitle:"", 
+            productPic:"",            
+            productProp:"Jочень вкусный",
+            productCost:0.00,
+            productEnable:true,
+            productPromo:false 
+
+        }
+    }
+    
     
 }
+
 </script>
 
 <style scoped>
+#mainCardDiv{
+    margin: 3px;
+    border-radius: 10px;
+    background-color: rgba(100,200, 200, 0.5);
+     box-shadow: gray;
+}
+
  #cardLogo{
-     background: url("../img/coffee/coffee1.jpg") no-repeat center top;
+     
+     /* background: url("../img/coffee/coffee1.jpg") no-repeat center top; */
       height: 50px;
       width: 50px; 
-    float: left;
+  
+    margin: 3%;
     background-size: cover;
+    border-radius: 20%;
  }
 
-
+img {
+  min-width: 100%;
+    
+    }
 
 </style>
--- src/products/products.vue
+++ src/products/products.vue
@@ -7,14 +7,14 @@
 
         </div>
 
-            <div class="goodsBody"> 
+        <div class="goodsBody"> 
         <div class="row ">
-              <product_card> </product_card>
-             <product_card> </product_card>
-                          <product_card> </product_card>
-                                       <product_card> </product_card>
-                                                    <product_card> </product_card>              
-            
+            <product_card v-for="Item in products" :key="Item.id" :Item="Item" > 
+
+
+            </product_card>
+                     
+
         </div>
     </div>   
        
@@ -31,13 +31,33 @@
 </template>
 
 <script>
-import product_card from "./product_card.vue"
+    import product_card from "./product_card.vue";
+
+
     export default {
         components: {product_card
         },
+        created() {
+            for (let i = 0; i < 9; i++) { 
+                // выведет 0, затем 1, затем 2
+                this.products.push({
+                    cardId:i,
+                    productType:"Prod"+i,
+                    productId:"productId"+i,
+                    productTitle:"Продукт № "+i,
+                    productPic:"../src/img/coffee/coffee"+(i+1)+".jpg",            
+                    productProp:i+") очень вкусный",
+                    productCost:i*100.3,
+                    productEnable:true,
+                    productPromo:false
+                });
+            };
+            console.log(this.products);
+
+        },
         data : function() {
             return {
-                mess: "fff"
+                products:[]
             }
         }
     
